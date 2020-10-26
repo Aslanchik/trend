a0f67b042c1fcbc4343d0684a3194a2e78714dd8
aslanchik <aslanchikgw2@hotmail.com>
2020-10-26 08:56:28 +0200

    implemented routing and semantic ui navbar

--- client/src/ApolloProvider.jsx
+++ client/src/ApolloProvider.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ApolloClient, InMemoryCache, createHttpLink, ApolloProvider} from "@apollo/client";
 
+// Create link to apollo server
 const httpLink = createHttpLink({
     uri:"http://localhost:8181"
 })
 
+// Init Apollo client with link to server
 const client = new ApolloClient({
     link:httpLink,
     cache: new InMemoryCache()
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import {BrowserRouter, Route} from 'react-router-dom';
+import {Container} from "semantic-ui-react";
+
+import './styles/main.scss';
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import Home from "./components/main/Home"
+import Navbar from "./components/navigation/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <BrowserRouter>
+      <Container>
+    <header>
+      <Navbar/>
+    </header>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/login" component={Login}/>
+    <Route exact path="/register" component={Register}/>
+
+    </Container>
+    </BrowserRouter>
   );
 };
 
