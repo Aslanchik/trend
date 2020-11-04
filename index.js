e722dd39ddd68e254115552758e2726673bd5a3d
aslanchik <aslanchikgw2@hotmail.com>
2020-11-04 20:58:19 +0200

    still troubleshooting

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = new ApolloServer({
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, "/client/build"));
+  app.use(express.static(__dirname, "client/build"));
 }
 
 const PORT = process.env.PORT || 8181;
